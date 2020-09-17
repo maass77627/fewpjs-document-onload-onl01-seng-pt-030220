6612e6b717167f11b35a10beb2ba1acd42282b18
kelly r maass <kmaass77627@gmail.com>
2020-09-17 07:23:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function(){
   console.log("The DOM has loaded");
+  let document.getElementById(`text`).innerHTML = "This is really cool!"
 });
 
 console.log("hello");
